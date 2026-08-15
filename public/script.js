3df3cfe3f50521dc0f1ac32c3f18f91f137749fd
Arpan Das <185752118+arpan-das01@users.noreply.github.com>
2026-08-15 23:52:50 +0530

    feat: add logic to send number to server

--- public/script.js
+++ public/script.js
@@ -1,7 +1,18 @@
 const form = document.getElementById("input-area")
-const input = document.getElementById("random-number"); 
+const input = document.getElementById("random-number");
+const url = new URL("http://127.0.0.1:3000/");
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
     event.preventDefault(); // prevents refreshing page after value submission
-    console.log("value submitted");
+    console.log("value submitted 1");
+    let numberValue = input.value;
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "text/plain",
+        },
+        body: numberValue,
+    })
+    
+    console.log("value submitted 2");
 });
