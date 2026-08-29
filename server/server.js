6a6cbd96803daf923657c6e9ce24d2cc06bcc5ab
Arpan Das <185752118+arpan-das01@users.noreply.github.com>
2026-08-29 23:59:42 +0530

    feat: add logic to store data from request stream

--- server/server.js
+++ server/server.js
@@ -4,19 +4,26 @@ import { EventEmitter } from 'node:events';
 
 const hostname = '127.0.0.1';
 const port = 3000;
-const getNumberEvent = new EventEmitter();
+//const getNumberEvent = new EventEmitter();
 
-getNumberEvent.on("receive-number", (numberString) => {
+/*getNumberEvent.on("receive-number", (numberString) => {
     const numberValue = Number(numberString);
     console.log("received a number\nValue:", numberValue);
-});
+});*/
 
 const server = createServer((req, res) => {
-  const {url, headers, method} = req;
-
-  if(!req.body){
-    getNumberEvent.emit("receive-number", req.body);
-  }
+  const {url, headers, method} = req; // why url, headers, method dark blue, fix later
+  var body = [];
+  var numberValue = Number(body);
+  
+  req.on("data", (chunk) => { // this part will store req body in body array
+    body.push(chunk);
+  })
+  .on("end", () =>{
+    body = Buffer.concat(body).toString();
+  });
+  
+  console.log("received a number\nValue:", numberValue);
   
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
