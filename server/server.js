e4ffcc4080396716ecaa3193116ac3871612c8ff
Arpan Das <185752118+arpan-das01@users.noreply.github.com>
2026-08-31 02:10:11 +0530

    fix: rewrite logic to receive data from request stream & print in terminal

--- server/server.js
+++ server/server.js
@@ -1,34 +1,36 @@
 import { createServer } from 'node:http';
-import { EventEmitter } from 'node:events';
-
 
 const hostname = '127.0.0.1';
 const port = 3000;
-//const getNumberEvent = new EventEmitter();
-
-/*getNumberEvent.on("receive-number", (numberString) => {
-    const numberValue = Number(numberString);
-    console.log("received a number\nValue:", numberValue);
-});*/
 
 const server = createServer((req, res) => {
-  const {url, headers, method} = req; // why url, headers, method dark blue, fix later
-  var body = [];
-  var numberValue = Number(body);
+  let body = [];
   
+  req.on("error", (err) => {
+    console.error(err);
+    res.statusCode = 400;
+    res.end();
+  });
+  res.on("error", (err) => {
+    console.error(err);
+  })
+
   req.on("data", (chunk) => { // this part will store req body in body array
     body.push(chunk);
   })
   .on("end", () =>{
     body = Buffer.concat(body).toString();
+    console.log("request stream ended");
+    let numberValue = Number(body);
+    console.log("received a number\nValue:", numberValue);
+    
+    if(typeof(numberValue) === "number"){
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+      res.end("received number");
+    }
   });
-  
-  console.log("received a number\nValue:", numberValue);
-  
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
-  res.end("received number");
 });
 
 server.listen(port, hostname, () => {
